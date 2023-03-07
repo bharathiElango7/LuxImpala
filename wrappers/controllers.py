36602a8c40a21f258561618b01979661d95dd6e2
narenelango <narenq7@gmail.com>
2023-03-08 01:22:40 +0530

    Wrote action masks for factoriy actions including a dfs

--- wrappers/controllers.py
+++ wrappers/controllers.py
@@ -5,6 +5,7 @@ import numpy as np
 import numpy.typing as npt
 from gym import spaces
 from lux.config import EnvConfig
+import math
 
 # Controller class copied here since you won't have access to the luxai_s2 package directly on the competition server
 class Controller:
@@ -72,7 +73,24 @@ class SimpleUnitDiscreteController(Controller):
         self.no_op_dim_high = self.dig_dim_high + self.no_op_dims
 
         self.total_act_dims = self.no_op_dim_high
-        action_space = spaces.Discrete(self.total_act_dims)
+        action_space = self.get_action_space(self.Mapsize)
+        self.RobotActions = []
+        for movingaction in DIRECTIONS:
+            self.RobotActions.append("Move" + movingaction)
+        for transferaction in DIRECTIONS:
+            for resource in RESOURCES:
+                self.RobotActions.append("Transfer" + resource + transferaction)
+        # for pickupaction in RESOURCES:
+        # RobotActions.append("Pickup"+pickupaction)
+        # pickup only power from factory
+        self.RobotActions.append("PickupPower")
+        self.RobotActions.append("Dig")
+        # Move center is NO-OP for a robot
+
+        self.TotalRobotActions = len(self.RobotActions)
+
+        self.FactoryActions = ["BuildLightRobot", "BuildHeavyRobot", "WaterForLichen", "NO-OP"]
+        self.TotalFactoryActions = len(self.FactoryActions)
 
         super().__init__(action_space)
     def get_action_space(self,MapSize:int):
@@ -81,39 +99,28 @@ class SimpleUnitDiscreteController(Controller):
         P=2
         # Transfer all resources in cargo -  to make it discrete - otherwise too many actions
         # Pickup upto cargo capacity - to make it discrete - otherwise too many actions
-        RobotActions =[]
-        for movingaction in DIRECTIONS:
-            RobotActions.append("Move"+movingaction)
-        for transferaction in DIRECTIONS:
-            for resource in RESOURCES:
-                RobotActions.append("Transfer"+resource+transferaction)
-        #for pickupaction in RESOURCES:
-            #RobotActions.append("Pickup"+pickupaction)
-        #pickup only power from factory
-        RobotActions.append("PickupPower")
-        RobotActions.append("Dig")
-        # Move center is NO-OP for a robot
 
-        TotalRobotActions = len(RobotActions)
-
-        FactoryActions = ["BuildLightRobot","BuildHeavyRobot","WaterForLichen","NO-OP"]
-        TotalFactoryActions = len(FactoryActions)
         action_space = spaces.Dict({
-            "Robot": spaces.MultiDiscrete(np.zeros((1, P, x, y), dtype=int) + TotalRobotActions),
-            "Factory": spaces.MultiDiscrete(np.zeros((1, P, x, y), dtype=int) + TotalFactoryActions)
+            "LightRobot": spaces.MultiDiscrete(np.zeros((1, P, x, y), dtype=int) + self.TotalRobotActions),
+            "HeavyRobot": spaces.MultiDiscrete(np.zeros((1, P, x, y), dtype=int) + self.TotalRobotActions),
+            "Factory": spaces.MultiDiscrete(np.zeros((1, P, x, y), dtype=int) + self.TotalFactoryActions)
 
         })
+
+        return action_space
 
     def is_move_action(self, id):
         return id < self.move_dim_high
     def get_move_action(self, id):
         # move direction is id + 1 since we don't allow move center here
+        # keep moving forever until change of direction
         return np.array([0, id+1, 0, 0, 0, 9999])
 
     def is_transfer_action(self, id):
         return id < self.transfer_dim_high
     def get_transfer_action(self, direction,resource, amount):
-        return np.array([1, direction, resource, amount, 0, 9999])
+        # its a one step action, no use of repeating
+        return np.array([1, direction, resource, amount, 0, 1])
 
     def is_pickup_action(self, id):
         return id < self.pickup_dim_high
@@ -124,7 +131,8 @@ class SimpleUnitDiscreteController(Controller):
     def get_pickup_action(self, amount):
         # max battery capacity is 3000 for heavy robot, so pick up max amount of power
         # 4 means power
-        return np.array([2, 0, 4, 3000, 0, 9999])
+        # its a one step action, no use of repeating
+        return np.array([2, 0, 4, 3000, 0, 1])
 
     def get_dig_action(self, id):
         # keep diggin until cargo is full
@@ -145,22 +153,30 @@ class SimpleUnitDiscreteController(Controller):
             units = shared_obs["units"][agent]
             for unit_id in units.keys():
                 unit = units[unit_id]
+                if unit["unit_type"] == "LIGHT":
+                    unitType = "LightRobot"
+                else:
+                    unitType = "HeavyRobot"
                 cargo_space = env_cfg.ROBOTS[unit["unit_type"]].CARGO_SPACE
                 battery_cap = env_cfg.ROBOTS[unit["unit_type"]].BATTERY_CAPACITY
                 x,y = unit["pos"]
-                choice = actionTensor["Robot"][0][P][x][y]
+                choice = actionTensor[unitType][0][P][x][y]
                 action_queue = []
                 no_op = False
                 if self.is_move_action(choice):
                     action_queue = [self.get_move_action(choice)]
                 elif self.is_transfer_action(choice):
+                    # there are 25 transfer actions
+                    # assign 5 for each resource
                     id = choice
                     id = id - self.move_dim_high
                     direction = id % 5
                     resource = int(id / 5)
+                    # transfer all resources in cargo
                     amount = unit["cargo"][resource]
                     action_queue = [self.get_transfer_action(direction,resource,amount)]
                 elif self.is_pickup_action(choice):
+                    # pickup only power from factory
                     chargeleft = battery_cap - unit["power"]
                     action_queue = [self.get_pickup_action(chargeleft)]
                 elif self.is_dig_action(choice):
@@ -189,50 +205,70 @@ class SimpleUnitDiscreteController(Controller):
 
         return FinalAction
 
+    # start from center of factory and do a dfs
+    # subtract 9 from result.
+    # this will number of connected lichen tiles + new lichen tiles
+    def dfs(self,board,visited,x,y,strainId):
+        if x<0 or y<0 or x>=self.Mapsize or y>=self.Mapsize:
+            return 0
+        if (x,y) in visited or board["rubble"][x][y] !=0 or board["ice"][x][y] !=0 or board["ore"][x][y] !=0:
+            return 0
+        if board["lichen_strains"][x][y] != strainId:
+            # new lichen tile to be be planted
+            visited.add((x, y))
+            return 1
+        visited.add((x,y))
+        # if current cell has lichen, continue dfs
+        return 1 + self.dfs(board,visited,x+1,y,strainId) + self.dfs(board,visited,x-1,y,strainId) \
+            +self.dfs(board,visited,x,y+1,strainId) + self.dfs(board,visited,x,y-1,strainId)
 
+    def actionMask2(self,obs):
+        actionspace = self.get_action_space(self.Mapsize)
 
-
-
-
-    def action_to_lux_action(
-        self, agent: str, obs: Dict[str, Any], action: npt.NDArray ):
+        # create an actions mask nd array with all actions allowed and set to true.
+        # last dimension is number of possible actions for each unit type
+        available_actions_mask = {
+            key: np.ones(space.shape + (self.TotalRobotActions if key == "Robot" else self.TotalFactoryActions,), dtype=bool)
+            for key, space in actionspace.spaces.items()
+        }
         shared_obs = obs["player_0"]
-        lux_action = dict()
-        units = shared_obs["units"][agent]
-        for unit_id in units.keys():
-            unit = units[unit_id]
-            choice = action
-            action_queue = []
-            no_op = False
-            if self._is_move_action(choice):
-                action_queue = [self._get_move_action(choice)]
-            elif self._is_transfer_action(choice):
-                action_queue = [self._get_transfer_action(choice)]
-            elif self._is_pickup_action(choice):
-                action_queue = [self._get_pickup_action(choice)]
-            elif self._is_dig_action(choice):
-                action_queue = [self._get_dig_action(choice)]
+        Agents = ["player_0", "player_1"]
+
+        for agent in Agents:
+            lux_action = dict()
+            if agent == "player_0":
+                P = 0
             else:
-                # action is a no_op, so we don't update the action queue
-                no_op = True
-
-            # simple trick to help agents conserve power is to avoid updating the action queue
-            # if the agent was previously trying to do that particular action already
-            if len(unit["action_queue"]) > 0 and len(action_queue) > 0:
-                same_actions = (unit["action_queue"][0] == action_queue[0]).all()
-                if same_actions:
-                    no_op = True
-            if not no_op:
-                lux_action[unit_id] = action_queue
-
-            break
-
-        factories = shared_obs["factories"][agent]
-        if len(units) == 0:
+                P = 1
+            # factory actions - build robots only if metal cost + power cost is met
+            # 0 - light
+            # 1- heavy
+            # 2- water
+            # 3- no op
+            factories = shared_obs["factories"][agent]
             for unit_id in factories.keys():
-                lux_action[unit_id] = 1  # build a single heavy
+                factory = factories[unit_id]
+                x,y = factory["pos"]
+                # check if factory has enough metal and power to build a robot
+                if factory["power"]<50 or factory["metal"]<10:
+                    available_actions_mask["Factory"][0][P][x][y][0] = False
+                    available_actions_mask["Factory"][0][P][x][y][1] = False
+                elif factory["power"] < 500 or factory["metal"] < 100:
+                    available_actions_mask["Factory"][0][P][x][y][1] = False
+                strainId = factory["strain_id"]
+                water = factory["water"]
+                visited = set()
+                waterRequiredForLichen = math.ceil((self.dfs(shared_obs["board"],visited,x,y,strainId)-9)/10)
+                if water < waterRequiredForLichen:
+                    available_actions_mask["Factory"][0][P][x][y][2] = False
 
-        return lux_action
+            # robot actions
+
+
+
+
+
+
 
     def action_masks(self, agent: str, obs: Dict[str, Any]):
         """
